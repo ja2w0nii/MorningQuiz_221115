91953ae0fa3c91b16d74890854071bdad9d71d6c
jaewon <jaewoniik94@gmail.com>
2022-11-15 09:34:55 +0900

    Morning Quiz 221115

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class UserRegistrationTest(APITestCase):
+    def test_registration(self):
+        url = reverse("user_view")
+        user_data = {
+            "username": "testuser",
+            "fullname": "testuser",
+            "email": "test@testuser.com",
+            "password": "password", 
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.data, {"message": "가입 완료!!"})
